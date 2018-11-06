47e661e807068f8f0331293100f9bae620001cae
Leonardo Magalhães Andrade <leonardo.magalhaes@ndd.com.br>
2018-11-06 11:07:46 -0200

    Adicionado solucao e projetos

---
+++ src/ProjetoDesempenho/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjetoDesempenho
+{
+    public class Orcamento
+    {
+        private double valor;
+        public Orcamento(double valor)
+        {
+            this.valor = valor;
+        }
+        public double getValor()
+        {
+            return valor;
+        }
+    }
+
+    public class Imposto
+    {
+        public Imposto()
+        {
+        }
+        public double calcula(Orcamento orcamento)
+        {
+            Console.WriteLine("teste");
+            return orcamento.getValor() * 0.1;
+        }
+    }
+}
---
+++ src/ProjetoDesempenhoTeste/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using ProjetoDesempenho;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Orcamento orcamento = new Orcamento(100.0);
+            Imposto imposto = new Imposto();
+            double result = imposto.calcula(orcamento);
+            // valor esperado, valor obtido
+            Assert.AreEqual(10.0, result, double.MinValue);
+        }
+    }
+}
